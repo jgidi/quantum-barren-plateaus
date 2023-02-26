f217f5a97aec3d8a897c0425db7c1c7d89e8270f
fequinteros <fequinteros.moro@gmail.com>
2023-02-26 19:16:59 -0300

    commented code 26

--- qubap/qiskit/felipe/state_efficient_ansatz.py
+++ qubap/qiskit/felipe/state_efficient_ansatz.py
@@ -3,7 +3,21 @@ from qiskit.circuit import ParameterVector
 
 import numpy as np
 
-def cnot_layer( n_qbits, n_cnot='Full_connect' ):   
+def cnot_layer( n_qbits, n_cnot='Full_connect' ):
+    """
+    Create an entangling layer circuit.
+
+    Input:
+        -n_qbits: Number of qubits of the circuit
+        
+        -n_cnot:  Specify the type of entangling layer:
+                    -Could be 'Full_connect' that means the circuit is full fill with CNOT's gates. 
+                    -A intenger number that indicates the number of CNOT's to implement from the first qubit to the last of the first half of qubits in descendet order.
+                    -A list of pair list e.g. [[1,2], [7,6]] where the first element of the array is the control qubit e.g 1, 7 and the second qubit of the list the target qubit e.g 2, 6.
+
+    Output:
+        - The entangling layer as a Quantum Circuit.
+    """ 
     sysA = int( n_qbits/2 )
     circ = QuantumCircuit( n_qbits )
         
@@ -24,6 +38,15 @@ def cnot_layer( n_qbits, n_cnot='Full_connect' ):
 
 
 def parity_test( n ):
+    """
+    Check if the number is even or odd.
+
+    Input:
+        -n: A number.
+
+    Output:
+        -Return 0 if the number is even or -1 if the number is odd.
+    """
     if n%2 == 0:
         p = 0
     else:
@@ -32,9 +55,26 @@ def parity_test( n ):
     return p
 
 
-def _GateSmith( params, qubits, n_qbits_crz=2, deep=1, name=None ):
-      
-    qubits= len( qubits )
+def SCL( params, n_qbits, n_qbits_crz=2, deep=1, name=None ):
+    """
+    Schmith Coefficient Layer.
+
+    Input:
+        -params:      The parameters to initialize the circuit.
+
+        -n_qbits:     The number of qubits of the circuit.
+        
+        -n_qbits_crz: How many qubits are used in the control Z gate. Default is two; one control and one target.
+        
+        -deep:        The deep of the circuit or how many times the circuit is repeated. Default is one.
+        
+        -name:        The name of the circuit. Default None.
+
+    Output:
+        -A Parametric Quantum Circuit (PQC) that in our case could perform a schmith decomposition or a basis change.
+    """
+
+    qubits= len( n_qbits )
     par = parity_test( qubits )
     qc = QuantumCircuit( qubits, name=name )
         
@@ -62,44 +102,50 @@ def _GateSmith( params, qubits, n_qbits_crz=2, deep=1, name=None ):
     return qc.to_gate( )
 
 
+def ansatz_constructor( n_qbits, unitaries=[SCL, SCL, SCL], n_qb_crz=[2,2,2], deep= [1,1,1], n_cnot='Full_connect' ):
+    """
+    The ansatz circuit that are made of three Parametrized Quantum Circuits (PQC's) and one entangling layer. The input parameters will define this four parts of the ansatz circuit.
 
-def SCL( params, qubits, n_qbits_crz=2, deep=1, name=None ):
-    "Schmidt Coefficient Layer"
-    #assert ( (qubits[-1]+1)%2 == 0 ) and ( type(qubits) == list ), "The qubits must be given in a list and the total number of qubits must be an even integer."
+    Input:
+        -n_qbits:     The number of qubits of the circuit.
+        
+        -unitaries:   A list of three PQC's
+        
+        -n_qbits_crz: A list containing how many qubits are used in the control Z gate for each PQC's. Default are [2,2,2].
+        
+        -deep:        A list containing the deep of each PQC. Default is [1,1,1].
 
-    #self.append( _GateSmith( params, qubits, n_qbits_crz=n_qbits_crz, deep=deep, name=name ), qubits )
-    qc = _GateSmith( params, qubits, n_qbits_crz=n_qbits_crz, deep=deep, name=name )
+        -n_cnot:  Specify the type of entangling layer:
+                    -Could be 'Full_connect' that means the circuit is full fill with CNOT's gates. 
+                    -A intenger number that indicates the number of CNOT's to implement from the first qubit to the last of the first half of qubits in descendet order.
+                    -A list of pair list e.g. [[1,2], [7,6]] where the first element of the array is the control qubit e.g 1, 7 and the second qubit of the list the target qubit e.g 2, 6.
 
-    return qc 
 
+    Output:
+        -A Parametric Quantum Circuit (PQC) that in our case could perform a schmith decomposition or a basis change.
+    """
 
-
-def ansatz_constructor( qubits, unitaries=[SCL, SCL, SCL], n_qb_crz=[2,2,2], deep= [1,1,1], n_cnot='Full_connect' ):
-    
-    #assert isinstance( n_cnot, int ) or isinstance( n_cnot, list ) or ( n_cnot == 'Full_connect' ), "The variable n_cnot must be of type integer or a numpy.ndarray"
-    #assert isinstance( unitaries, list ), "The unitaries must be given in a numpy.ndarray variable"
-    
-    num_params_SCL = ( deep[0] +1 )*qubits
-    
-    
+    num_params_SCL = ( deep[0] +1 )*n_qbits
+      
     params_1 = ParameterVector(r"$\theta$", num_params_SCL)
     params_2 = ParameterVector(r"$\phi$", num_params_SCL)
     params_3 = ParameterVector(r"$\omega$", num_params_SCL)
-    qc     = QuantumCircuit( qubits )
+    qc     = QuantumCircuit( n_qbits )
     
 
-    U_1   = unitaries[0]( params_1, range( int(qubits/2) ), n_qbits_crz=int(n_qb_crz[0]), deep=deep[0], name="U1" )
-    U_2   = unitaries[1]( params_2, range( int(qubits/2) ), n_qbits_crz=int(n_qb_crz[1]), deep=deep[1], name="U2" )
-    U_3   = unitaries[2]( params_3, range( int(qubits/2) ), n_qbits_crz=int(n_qb_crz[2]), deep=deep[2], name="U3" )
-    ent_l = cnot_layer( qubits, n_cnot=n_cnot )
+    U_1   = unitaries[0]( params_1, range( int(n_qbits/2) ), n_qbits_crz=int(n_qb_crz[0]), deep=deep[0], name="U1" )
+    U_2   = unitaries[1]( params_2, range( int(n_qbits/2) ), n_qbits_crz=int(n_qb_crz[1]), deep=deep[1], name="U2" )
+    U_3   = unitaries[2]( params_3, range( int(n_qbits/2) ), n_qbits_crz=int(n_qb_crz[2]), deep=deep[2], name="U3" )
+    ent_l = cnot_layer( n_qbits, n_cnot=n_cnot )
 
-    qc.compose( U_1 , qubits=range( int(qubits/2) ), clbits=None, inplace=True )         # Compose entangled layer cnots
+
+    qc.compose( U_1 , qubits=range( int(n_qbits/2) ), clbits=None, inplace=True )         # Compose entangled layer cnots
     qc.barrier()
     
-    qc.compose( ent_l , qubits=range( qubits ), clbits=None, inplace=True )              # Compose entangled layer cnots
+    qc.compose( ent_l , qubits=range( n_qbits ), clbits=None, inplace=True )              # Compose entangled layer cnots
     qc.barrier()
     
-    qc.compose( U_2 , qubits=range( int(qubits/2) ), clbits=None, inplace=True )         # Compose entangled layer cnots
-    qc.compose( U_3 , qubits=range( int(qubits/2), qubits ), clbits=None, inplace=True ) # Compose entangled layer cnots
+    qc.compose( U_2 , qubits=range( int(n_qbits/2) ), clbits=None, inplace=True )         # Compose entangled layer cnots
+    qc.compose( U_3 , qubits=range( int(n_qbits/2), n_qbits ), clbits=None, inplace=True ) # Compose entangled layer cnots
     
     return qc
