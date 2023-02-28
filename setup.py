117f24348128d76e01f53f80a7f44ed6755110f1
Jorge Gidi <jorgegidi@gmail.com>
2023-02-28 02:15:31 -0300

    installation requirements

--- setup.py
+++ setup.py
@@ -4,4 +4,9 @@ from setuptools import setup, find_packages
 setup(
     name = 'qubap',
     packages = find_packages(),
+    install_requires = [
+        "numpy",
+        "qiskit",
+        "qiskit_aer",
+    ],
 )
